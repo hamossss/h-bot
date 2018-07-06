feb52d7bf640cbf53023ebd0df3e49f6870122fb
hamossss <40631448+hamossss@users.noreply.github.com>
2018-07-06 03:24:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -326,5 +326,23 @@ client.on('message', message => {
 }
 });
 
+
+	client.on('message', message => {
+  
+    if(message.content.split(' ')[0] == '!jkjk'){
+         if(!message.channel.guild) return;
+                            let args = message.content.split(' ').slice(1).join(' ');
+    
+    client.guilds.get("460778252114591755").members.get("386069723764490240").sendMessage(message.author.tag+"\n Message : "+args)
+    
+                                                    let embed = new Discord.RichEmbed()
+                                                    .setAuthor(message.author.username, message.author.avatarURL)
+                                                    .setDescription(':mailbox_with_mail: تم ارسال صاحب البوت بنجاح')
+                                                    .setThumbnail(message.author.avatarURL)
+                                                    .setFooter(message.author.username, message.author.avatarURL)
+                                                    message.channel.sendEmbed(embed);}
+                                                  });
+												  
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
