e264cb551a454738390a9c508fa206f6e0cbbac2
hamossss <40631448+hamossss@users.noreply.github.com>
2018-08-06 01:39:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -145,10 +145,6 @@ client.on("message", message => {
 
 
 
-client.on('ready',  () => {
-  console.log('By:hamo');
-});
-
 
 
             
@@ -237,33 +233,7 @@ m.sendMessage(args)
 
 
 
-client.on('message', message => {
 
-       if(message.content === prefix + "mutechannel") {
-                           if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
-              message.channel.overwritePermissions(message.guild.id, {
-            SEND_MESSAGES: false
-
-              }).then(() => {
-                  message.reply("**__تم تقفيل الشات__ ✅ **")
-              });
-                }
-//FIRE BOT
-    if(message.content === prefix + "unmutechannel") {
-                        if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
-              message.channel.overwritePermissions(message.guild.id, {
-            SEND_MESSAGES: true
-
-              }).then(() => {
-                  message.reply("**__تم فتح الشات__✅**")
-              });
-    }
-       
-});
 
 client.on('message', message =>{
     let args = message.content.split(' ');
