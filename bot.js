0a60fda36a0dde3c3a803964977412398d76f4e1
hamossss <40631448+hamossss@users.noreply.github.com>
2018-08-11 11:54:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,33 +12,7 @@ client.on('message', message => {
 });
 
 
-const jimp = require('jimp');   
-const canvas = require("canvas");
 
-
-
-
-
-
-
-const sWlc = {}
-const premium = ['475803379214581763']
-client.on('message', message => {
-var prefix = "!";
-if(message.channel.type === "dm") return;
-if(message.author.bot) return;
-  if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
-    channel: "welcome"
-}
-const channel = sWlc[message.guild.id].channel
-  if (message.content.startsWith(prefix + "setwelcomer")) {
-    if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
-    let newChannel = message.content.split(' ').slice(1).join(" ")
-    if(!newChannel) return message.reply(`**${prefix}setwelcomer <channel name>**`)
-    sWlc[message.guild.id].channel = newChannel
-    message.channel.send(`**${message.guild.name}'s channel has been changed to ${newChannel}**`);
-  }
-});
  
 
 
